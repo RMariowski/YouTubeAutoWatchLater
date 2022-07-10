fb0fddeb3cbd942278120b9a55b6d6ed666ffdfe
Radek Mariowski <rmariowski@gmail.com>
2022-07-10 16:57:16 +0200

    Enable implicit usings

--- YouTubeAutoWatchLater/GlobalUsings.cs
+++ YouTubeAutoWatchLater/GlobalUsings.cs
@@ -1,9 +1,4 @@
-﻿global using System;
-global using System.Linq;
-global using System.Threading.Tasks;
-global using System.Collections.Generic;
-
-global using Google.Apis.YouTube.v3;
+﻿global using Google.Apis.YouTube.v3;
 global using Google.Apis.YouTube.v3.Data;
 
 global using YouTubeApi = Google.Apis.YouTube.v3.YouTubeService;
