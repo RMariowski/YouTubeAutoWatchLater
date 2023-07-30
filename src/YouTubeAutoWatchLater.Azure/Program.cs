b65b0c2311f0185a5b4424b53aaf6add4638cc6c
Radek Mariowski <rmariowski@gmail.com>
2023-07-30 12:24:36 +0200

    Update nuget packages

--- src/YouTubeAutoWatchLater.Azure/Program.cs
+++ src/YouTubeAutoWatchLater.Azure/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using YouTubeAutoWatchLater.Azure.Repositories;
 using YouTubeAutoWatchLater.Core;
 using YouTubeAutoWatchLater.Core.Repositories;
@@ -12,20 +13,31 @@ internal class Program
     private static async Task Main(string[] args)
     {
         var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults((_, builder) =>
-            {
-                builder
-                    .AddApplicationInsights()
-                    .AddApplicationInsightsLogger();
-            })
+            .ConfigureFunctionsWorkerDefaults()
             .ConfigureServices((hostContext, services) =>
             {
+                services.AddApplicationInsightsTelemetryWorkerService();
+                services.ConfigureFunctionsApplicationInsights();
+
                 var configuration = hostContext.Configuration;
 
                 services
                     .AddCore(configuration)
                     .AddScoped<IConfigurationRepository, ConfigurationTableStorageRepository>()
                     .AddScoped<IAutoAddedVideosRepository, AutoAddedVideosTableStorageRepository>();
+            })
+            .ConfigureLogging(logging =>
+            {
+                logging.Services.Configure<LoggerFilterOptions>(options =>
+                {
+                    var defaultRule = options.Rules.FirstOrDefault(rule =>
+                        rule.ProviderName ==
+                        "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+                    if (defaultRule is not null)
+                    {
+                        options.Rules.Remove(defaultRule);
+                    }
+                });
             })
             .Build();
         await host.RunAsync();
